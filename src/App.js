ac3c9396a14d0542bd2dd07bdea73e6b0f4a1faa
jhonathan s. melo <90641508+jonhbass@users.noreply.github.com>
2023-06-05 21:26:19 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import NavBar from "./componets/NavBar/NavBar";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <h1>primeiro projeto react</h1>
+      <h2>kanban</h2>
+    </div>
+  );
+}
---
+++ src/componets/NavBar/NavBar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./NavBar.css";
+
+export default function NavBar() {
+  return (
+    <nav className="NavBar">
+      <span> Lista de Tarefas </span>
+    </nav>
+  );
+}
